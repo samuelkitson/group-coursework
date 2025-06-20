c14d074709459c979ecbe1fb76d54d0b769b2d0d
samuelkitson <32879754+samuelkitson@users.noreply.github.com>
2025-06-20 14:31:21 +0100

    Add errorhandling middleware

---
+++ api/errors/errorMiddleware.js
@@ -0,0 +1,18 @@
+// Inspired by https://dev.to/aneeqakhan/setup-error-middleware-and-async-handler-299i
+const errors = require("./errors");
+
+// Catch and handle errors thrown during request handling. If the error thrown
+// if a CutsomError (with status code and message), return this. Otherwise, just
+// default to error 500.
+exports.errorHandler = (err, req, res, next) => {
+  if (err instanceof errors.CustomError) {
+    return res.status(err.status).json({ message: err.message });
+  }
+
+  res.status(500);
+  console.error(`Unhandled error occurred: ${err.message}`);
+  res.json({
+    message: "Sorry, an unknown error occurred. Please try again.",
+    details: err.message,
+  });
+};
