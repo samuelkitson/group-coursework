af7b99412998196cff8ab5686c78c8831e7693a7
samuelkitson <32879754+samuelkitson@users.noreply.github.com>
2025-07-01 16:09:06 +0100

    Add star ratings to peer review

--- frontend/src/features/checkin/PeerReviewForm.jsx
+++ frontend/src/features/checkin/PeerReviewForm.jsx
@@ -1,26 +1,34 @@
 import React, { memo, useEffect, useState } from "react";
-import { Accordion, Badge, Form } from "react-bootstrap";
-import { Check, Check2, HourglassSplit } from "react-bootstrap-icons";
+import { Accordion, Badge, Col, Form, Row } from "react-bootstrap";
+import { Check, Check2, HourglassSplit, Star, StarFill } from "react-bootstrap-icons";
 
 const PeerReviewForm = memo(({ index, answer, onChange }) => {
   const [BadgeIcon, setBadgeIcon] = useState(HourglassSplit);
   const [colour, setColour] = useState("danger");
+  const [hoveredSkill, setHoveredSkill] = useState(null);
 
   const handleAnswerChange = (event) => {
     const { name, value } = event.target;
     onChange(answer.recipient, name, value);
   };
 
-  const handleSkillChange = (event) => {
-    const { name, value } = event.target;
+  const handleSkillChange = (name, value) => {
     const updatedSkills = {...answer.skills};
     updatedSkills[name] = value;
     onChange(answer.recipient, "skills", updatedSkills);
-  }
+  };
+
+  const getStarColour = (value, starNumber) => {
+    if (starNumber > value) return "secondary";
+    if (value <= 1) return "danger";
+    if (value <= 3) return "warning"
+    if (value <= 5) return "success";
+    return "secondary";
+  };
 
   useEffect(() => {
     const blankRatings = Object.values(answer.skills).filter(s => !(s >= 1 && s <= 5));
-    if (answer.comment != "" && blankRatings.length === 0) {
+    if (answer.comment.length > 50 && blankRatings.length === 0) {
       setBadgeIcon(Check2);
       setColour("success");
     } else {
@@ -37,28 +45,45 @@ const PeerReviewForm = memo(({ index, answer, onChange }) => {
       </Accordion.Header>
       <Accordion.Body>
         {Object.entries(answer.skills).map(([skillName, skillValue]) => (
-          <Form.Group>
-            <Form.Label>
+          <Form.Group as={Row} className="align-items-center">
+            <Form.Label column md={4} className={`mb-md-2 py-1 ${hoveredSkill===skillName && "fw-bold"}`}>
               {skillName}
             </Form.Label>
-            <Form.Range
-              min={1}
-              max={5}
-              name={skillName}
-              value={skillValue}
-              onChange={handleSkillChange}
-              className="me-3"
-            />
+            <Col
+              md={8}
+              onMouseEnter={() => setHoveredSkill(skillName)}
+              onMouseLeave={() => setHoveredSkill(null)}
+              className="mb-3 mb-md-2"
+            >
+              {[1, 2, 3, 4, 5].map((i) => {
+                const filled = i <= (skillValue);
+                const StarIcon = filled ? StarFill : Star;
+                return (
+                  <StarIcon
+                    key={i}
+                    size={24}
+                    className={`me-2 text-${getStarColour(skillValue, i)}`}
+                    role="button"
+                    onClick={() => handleSkillChange(skillName, i)}
+                  />
+                );
+              })}
+            </Col>
           </Form.Group>
         ))}
-        <Form.Group className="mb-4">
-          <Form.Label>
-            Review comment
+        <Form.Group className="mt-3">
+          <Form.Label className="text-muted">
+            Please give {answer.name} some feedback about their recent work.
+            What have they done well and how could they improve? Your comments
+            will be moderated and shared with them anonymously.
           </Form.Label>
           <Form.Control
-            value={answer.comment}
+            as="textarea"
             name="comment"
+            rows={3}
+            value={answer.comment}
             onChange={handleAnswerChange}
+            placeholder="Review comments"
           />
         </Form.Group>
       </Accordion.Body>
