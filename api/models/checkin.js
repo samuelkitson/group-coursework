77505fb33f22f5ee93b6926e19dee81fddbc03fb
samuelkitson <32879754+samuelkitson@users.noreply.github.com>
2025-07-01 17:41:26 +0100

    Remove old check-in code

--- api/models/checkin.js
+++ api/models/checkin.js
@@ -2,8 +2,6 @@ const { Schema, model } = require("mongoose");
 const assignment = require("./assignment");
 const peerReview = require("./peerReview");
 
-// New object format
-
 const fullReviewSchema = new Schema(
   {
     skills: { type: Object, of: Number, required: true, },
@@ -21,24 +19,6 @@ const checkinSchema = new Schema(
     reviews: { type: Object, of: fullReviewSchema, },
   },
   { _id: true, timestamps: true, },
-);
-
-
-
-
-const oldCheckinSchema = new Schema(
-  {
-    team: { type: "ObjectId", ref: "team", required: true, },
-    peerReview:  { type: "ObjectId", ref: "peerReview", required: true, },
-    effortPoints: {
-      type: Object,
-      of: {
-        type: Object, 
-        of: Number,
-      },
-      required: true,
-    },
-  },
 );
 
 checkinSchema.statics.findByTeamAndPeerReview = async function (
@@ -65,32 +45,6 @@ checkinSchema.statics.findByTeamsAndPeerReview = async function (
       peerReview: peerReviewId,
     }, 
   ).select(selectFields);
-};
-
-checkinSchema.statics.createNewCheckin = async function (
-  teamId,
-  peerReviewId,
-) {
-  const checkinDate = new Date();
-  let periodStart = new Date(checkinDate);
-  if (checkinDate.getDay() !== 1) {
-    // Not Monday today, so set start period back to previous Monday
-    // https://stackoverflow.com/a/46544455
-    periodStart.setDate(periodStart.getDate() - (periodStart.getDay() + 6) % 7);
-  }
-  periodStart.setHours(0, 0, 0);
-
-  const periodEnd = new Date(periodStart);
-  periodEnd.setDate(periodStart.getDate() + 6);
-  periodEnd.setHours(23, 59, 59); 
-
-  return await this.create({
-    team: teamId,
-    peerReview: peerReviewId,
-    periodStart,
-    periodEnd,
-    effortPoints: new Map(),
-  });
 };
 
 module.exports = model("checkin", checkinSchema);
