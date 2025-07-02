2bd835339a67171734fdd8afc8ff782f7311306f
samuelkitson <32879754+samuelkitson@users.noreply.github.com>
2025-07-02 15:58:00 +0100

    Use react-hook-form for some of meeting modal

--- frontend/src/features/meetings/NewMeetingModal.jsx
+++ frontend/src/features/meetings/NewMeetingModal.jsx
@@ -2,17 +2,24 @@ import { utcToSelectorFormat } from '@/utility/datetimes';
 import React, { useEffect, useState } from 'react';
 import { Modal, Button, Form, Table, Row, Col, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { CheckCircleFill, Eyeglasses, InfoCircle, SlashCircleFill, XCircleFill, XLg } from 'react-bootstrap-icons';
+import { useForm, useFormState } from 'react-hook-form';
 import toast from 'react-hot-toast';
 import Select from 'react-select';
 
 const NewMeetingModal = ({ showModal, onHide, teamMembers, supervisors, previousActions, onSubmit, existingMeeting }) => {
-  const [location, setLocation] = useState('');
-  const [dateTime, setDateTime] = useState('');
-  const [minutes, setMinutes] = useState('');
   const [attendance, setAttendance] = useState([]);
   const [prevActions, setPrevActions] = useState([]);
   const [newActions, setNewActions] = useState([]);
   const [editMode, setEditMode] = useState(false);
+
+  const defaultValues = {
+    location: "",
+    dateTime: "",
+    minutes: "",
+  };
+
+  const { control, register, reset, getValues, } = useForm({ defaultValues });
+  const { isDirty } = useFormState({ control });
 
   const teamMemberOptions = teamMembers.map(user => ({
     value: user._id,
@@ -70,6 +77,7 @@ const NewMeetingModal = ({ showModal, onHide, teamMembers, supervisors, previous
   };
 
   const handleSubmit = () => {
+    const { location, dateTime, minutes } = getValues();
     // Check if all fields are completed
     if (!location) return toast.error("Please add a meeting location.");
     if (!dateTime) return toast.error("Please add the meeting date and time.");
@@ -99,10 +107,11 @@ const NewMeetingModal = ({ showModal, onHide, teamMembers, supervisors, previous
           if (existingMeeting.attendance.absent.find(a => a._id === userId)) return "absent";
           return null;
         };
-
-        setLocation(existingMeeting.location);
-        setDateTime(utcToSelectorFormat(existingMeeting.dateTime));
-        setMinutes(existingMeeting.discussion);
+        reset({...defaultValues,
+          location: existingMeeting?.location ?? "",
+          dateTime: utcToSelectorFormat(existingMeeting?.dateTime) ?? "",
+          minutes: existingMeeting?.discussion ?? "",
+        });
         const supervisorsMarked = supervisors.map(s => ({...s, supervisor: true}));
         const memberAttendance = JSON.parse(JSON.stringify(teamMembers)).map(user => ({ ...user, status: getAttendanceById(user._id) ?? "absent"}));
         const supervisorAttendance = JSON.parse(JSON.stringify(supervisorsMarked)).map(user => ({ ...user, status: getAttendanceById(user._id) ?? "notneeded" }));
@@ -110,10 +119,7 @@ const NewMeetingModal = ({ showModal, onHide, teamMembers, supervisors, previous
         setPrevActions(existingMeeting.previousActions);
         setNewActions(existingMeeting.newActions.concat({ action: '', assignees: [] }));
       } else {
-        // New meeting
-        setLocation('');
-        setDateTime('');
-        setMinutes('');
+        reset( defaultValues );
         const supervisorsMarked = supervisors.map(s => ({...s, supervisor: true}));
         const memberAttendance = JSON.parse(JSON.stringify(teamMembers)).map(user => ({ ...user, status: "attended" }));
         const supervisorAttendance = JSON.parse(JSON.stringify(supervisorsMarked)).map(user => ({ ...user, status: "notneeded" }));
@@ -139,19 +145,19 @@ const NewMeetingModal = ({ showModal, onHide, teamMembers, supervisors, previous
           <Row className="mb-3 gy-3">
             <Col md={6}>
               <Form.Group className="form-floating">
-                <Form.Control type="text" id="location" placeholder="Location" value={location} onChange={e => setLocation(e.target.value)} />
+                <Form.Control type="text" id="location" placeholder="Location" {...register("location")} />
                 <Form.Label htmlFor="location">Meeting location</Form.Label>
               </Form.Group>
             </Col>
             <Col md={6}>
               <Form.Group className="form-floating">
-                <Form.Control type="datetime-local" id="dateTime" value={dateTime} onChange={e => setDateTime(e.target.value)} />
+                <Form.Control type="datetime-local" id="dateTime" {...register("dateTime")} />
                 <Form.Label htmlFor="dateTime">Date and time</Form.Label>
               </Form.Group>
             </Col>
           </Row>
 
-          <Form.Control className="mb-1" as="textarea" placeholder="Meeting minutes" rows={4} value={minutes} onChange={e => setMinutes(e.target.value)} />
+          <Form.Control className="mb-1" as="textarea" placeholder="Meeting minutes" rows={4} {...register("minutes")} />
           <p className="mb-3 small text-muted">Meeting minutes should give a summary of everything that you discussed and agreed upon. Include enough detail that someone who didn't attend can understand what happened. Bullet points work well!</p>
 
           <h6>
