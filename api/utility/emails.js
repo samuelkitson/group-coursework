d104e3eec0120d58292ddcf88c49b20e14beaa90
samuelkitson <32879754+samuelkitson@users.noreply.github.com>
2025-07-31 10:59:53 +0100

    More work on generalised email sending

--- api/utility/emails.js
+++ api/utility/emails.js
@@ -1,8 +1,15 @@
 const nodemailer = require("nodemailer");
-const { SMTP_SERVER, SMTP_PORT, SMTP_SECURE, SMTP_FROM_ADDRESS } = process.env;
-const { InvalidParametersError } = require("../errors/errors");
+const { BASE_URL, SMTP_SERVER, SMTP_PORT, SMTP_SECURE, SMTP_FROM_ADDRESS } = process.env;
+const { InvalidParametersError, ConfigurationError, CustomError } = require("../errors/errors");
 const ejs = require("ejs");
 const path = require("path");
+
+/**
+ * In any function that needs to send emails, first check that email sending is
+ * ready. Do this with an await call like: await emails.emailsReady;
+ */
+
+const homePageLink = `<a href='${BASE_URL}'>${BASE_URL}</a>`;
 
 const transporter = nodemailer.createTransport({
   host: SMTP_SERVER,
@@ -26,12 +33,15 @@ const transporterReadyPromise = new Promise((resolve) => {
   });
 });
 
-const sendGenericEmail = async ({ recipientEmail, recipientName, replyToEmail, subject, headerText, bodyText, }) => {
+const sendGenericEmail = async ({ recipientEmail, recipientName, replyToEmail, subject, headerText, bodyText, templateId, redHeader, }) => {
+  const ready = await transporterReadyPromise;
+  if (!ready) throw new CustomError("Email sending not configured", 500);
   if (!subject || !recipientEmail || !headerText || !bodyText)
     throw new InvalidParametersError("Missing required parameters to send email.");
+  const headerColour = redHeader ? "#B11717" : "#005C84";
   const rendered = await ejs.renderFile(
     path.join(__dirname, "..", "views", "emails", "genericEmail.ejs"),
-    { recipientEmail, recipientName, headerText, bodyText, },
+    { recipientEmail, recipientName, headerText, bodyText, templateId: templateId ?? "0-00", headerColour, },
   );
   const mailOptions = {
     from: SMTP_FROM_ADDRESS,
@@ -44,11 +54,36 @@ const sendGenericEmail = async ({ recipientEmail, recipientName, replyToEmail, s
 };
 
 /**
- * In any function that needs to send emails, first check that email sending is
- * ready. Do this with an await call like: await emails.emailsReady;
+ * Email are sent based on the templates below, each of which has an identifier.
+ * The format is X-YY, where X is the email category and YY is the specific
+ * template. The email categories are based on their triggers and are:
+ *   * 1: security event
+ *   * 2: assignment configuration change
+ *   * 3: team configuration change
  */
+
+const newSupervisorPlaceholderEmail = ({ supervisorEmail, staffUserEmail, assignmentName, }) => {
+  const templateId = "2-01";
+  if (!supervisorEmail || !staffUserEmail || !assignmentName)
+    throw new InvalidParametersError("Missing required parameters to send email.");
+  const bodyText = `You've been added as a supervisor on ${assignmentName}.<br />This group coursework is managed using a web-based system. Please go to ${homePageLink} to activate your account and get started.`;
+  sendGenericEmail({ recipientEmail: supervisorEmail, replyToEmail: staffUserEmail, subject: "Activate your supervisor account", headerText: "Activate your supervisor account", bodyText, templateId, })
+    .catch(err => {console.error(`Failed to send email ${templateId}: ${err}`)});
+};
+
+const newSupervisorExistingEmail = ({ supervisorEmail, supervisorName, staffUserEmail, assignmentName, }) => {
+  const templateId = "2-02";
+  if (!supervisorEmail || !supervisorName || !staffUserEmail || !assignmentName)
+    throw new InvalidParametersError("Missing required parameters to send email.");
+  const bodyText = `You've been added as a supervisor on ${assignmentName}.<br />You already have an account on the group coursework management system. When they're ready, you'll be able to view your teams at ${homePageLink}.`;
+  sendGenericEmail({ recipientEmail: supervisorEmail, recipientName: supervisorName, replyToEmail: staffUserEmail, subject: "You've been added as a supervisor", headerText: "You've been added as a supervisor", bodyText, templateId, })
+    .catch(err => {console.error(`Failed to send email ${templateId}: ${err}`)});
+};
+
 module.exports = {
   emailTransporter: transporter,
   emailsReady: transporterReadyPromise,
-  sendGenericEmail: sendGenericEmail,
+  sendGenericEmail,
+  newSupervisorPlaceholderEmail,
+  newSupervisorExistingEmail,
 };
