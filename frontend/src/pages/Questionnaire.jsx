6cb78961ebe82df8a567832b7f1ce8bfb1893ece
samuelkitson <32879754+samuelkitson@users.noreply.github.com>
2025-08-12 11:43:10 +0100

    Add warnings and help messages to skills questionnaire

--- frontend/src/pages/Questionnaire.jsx
+++ frontend/src/pages/Questionnaire.jsx
@@ -1,13 +1,13 @@
 import api from "@/services/apiMiddleware";
 import { useBoundStore } from "@/store/dataBoundStore";
 import React, { useEffect, useState } from "react";
-import { Button, Col, Form, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
+import { Button, Card, Col, Form, OverlayTrigger, Row, Tooltip } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 import "./style/Questionnaire.css";
 import toast from "react-hot-toast";
 import { EMOJI_RATINGS, toTitleCase } from "@/utility/helpers";
-import { InfoCircle } from "react-bootstrap-icons";
+import { ChevronRight, InfoCircle, QuestionCircle, QuestionCircleFill } from "react-bootstrap-icons";
 
 function Questionnaire() {
   const navigate = useNavigate();
@@ -78,15 +78,41 @@ function Questionnaire() {
 
   useEffect(refreshData, [selectedAssignment]);
 
+  if (!isLoading && skillsList?.length == 0) return (
+    <>
+      <Row className="mb-4">
+        <Col>
+          <h1>Skills questionnaire</h1>
+          <p className="text-muted">
+            The module team for {selectedAssignment.name} is getting ready to
+            allocate teams. 
+          </p>
+        </Col>
+      </Row>
+
+      <Card className="shadow">
+        <Card.Body>
+          <Card.Title className="d-flex align-items-center">
+            <QuestionCircleFill className="me-2" /> No questions found
+          </Card.Title>
+          <p className="text-muted mb-0">
+            No questions have been configured for this assignment. If you think
+            this is a mistake, please contact your lecturers.
+          </p>
+        </Card.Body>
+      </Card>
+    </>
+  )
+
   return (
     <>
       <Row className="mb-4">
         <Col>
-          <h1>Allocation Questionnaire</h1>
+          <h1>Skills questionnaire</h1>
           <p className="text-muted">
             The allocation questionnaire for {selectedAssignment.name} is open!
-            Please take 5 minutes to complete it accurately, as this will help
-            make sure that group allocations are as fair as possible.
+            Please take 5 minutes to tell us how confident you are in each of
+            these key skills.
           </p>
         </Col>
       </Row>
@@ -94,8 +120,8 @@ function Questionnaire() {
       <Row>
         <Col>
           <h3>
-            Skill Rating
-            <OverlayTrigger overlay={<Tooltip>Visible to module staff and your teammates</Tooltip>}>
+            Rate your skills
+            <OverlayTrigger overlay={<Tooltip>Visible to module staff.<br />Top skill visible to your teammates.</Tooltip>}>
               <InfoCircle className="ms-3" size={16} />
             </OverlayTrigger>
           </h3>
@@ -145,12 +171,34 @@ function Questionnaire() {
             <Button
               variant="primary"
               type="submit"
-              className="mt-3 w-100"
+              className="mt-3 d-flex align-items-center"
               disabled={isSaving}
             >
-              Save Ratings
+              Save ratings
+              <ChevronRight className="ms-2" />
             </Button>
           </Form>
+        </Col>
+      </Row>
+
+      <Row className="mt-5">
+        <Col md={8}>
+          <h6 className="d-flex align-items-center">
+            <QuestionCircleFill className="me-1" />
+            How is this data used?
+          </h6>
+          <p className="text-muted">
+            Staff can use these ratings to make teams with good coverage of the
+            key skills for this assignment. Your best skills will also be shown
+            to your team, meaning you're more likely to be given tasks that suit
+            you.
+          </p>
+          <p className="text-muted">
+            <span className="fw-semibold">It's important to answer honestly</span>,
+            otherwise you may be placed in a team that's missing key skills.
+            Your responses will be visible to staff alongside your historic
+            marks.
+          </p>
         </Col>
       </Row>
     </>
